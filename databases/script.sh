f6ddec221da2b4c0dc3b46c499d74e428f95bf80
Tipsukon Samode <tipsukon.cheer@mail.kmutt.ac.th>
2021-10-29 08:18:11 +0000

    [UPDATE] add k8s

--- databases/script.sh
+++ databases/script.sh
@@ -2,5 +2,5 @@
 
 #set -e: ถ้ามีอะไร error ให้ออกเลย ไม่ต้องทำต่อ
 set -e
-mongoimport --host localhost --db ratings \
-  --collection ratings --drop --file /docker-entrypoint-initdb.d/ratings_data.json
+mongoimport --host localhost --username $MONGODB_USERNAME --password $MONGODB_PASSWORD \
+  --db $MONGODB_DATABASE --collection ratings --drop --file /docker-entrypoint-initdb.d/ratings_data.json
